5898440d71d176f0953f315f43a0251b9b95a443
hvmzx <hamza.mebarek1@gmail.com>
2024-04-28 01:31:17 +0200

    feat: fixed errors and now supports output volume

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM python:3.10
 WORKDIR .
 RUN git clone https://github.com/ciromattia/kcc.git
 RUN python -m pip install --upgrade pip
-RUN apt-get update && apt-get install -y --no-install-recommends python3 python3-dev python3-pip libpng-dev libjpeg-dev p7zip-full python3-pyqt5
-RUN python -m pip install --upgrade -r kcc/requirements.txt
-RUN pip install watchdog
+RUN apt-get update && apt-get install -y --no-install-recommends python3 python3-dev python3-pip libpng-dev libjpeg-dev p7zip-full python3-pyqt5 unrar-free libgl1
+RUN pip install Cmake watchdog
+RUN pip install -r kcc/requirements.txt
 RUN wget https://archive.org/download/kindlegen_linux_2_6_i386_v2_9/kindlegen_linux_2.6_i386_v2_9.tar.gz
 RUN tar -xf kindlegen_linux_2.6_i386_v2_9.tar.gz "kindlegen"  
 RUN cp -R 'kindlegen' '/usr/local/bin/'
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import subprocess
+import subprocess, os
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
-path="mangas"
-print('Watching for folder ' + path + ' for new mangas')
+print('Monitoring folder mangas for new mangas in .cbz format')
+OPTIONS=os.getenv('OPTIONS') + " -o"
 
 class MyHandler(PatternMatchingEventHandler):
     def on_created(self, event):
         if not event.is_directory:
-            print("New file created:", event.src_path)
-            subprocess.run(["python", "kcc/kcc-c2e.py -p KoC -m -u -s -d -t " + event.src_path])
+            print("New manga detected :", event.src_path)
+            command="python kcc/kcc-c2e.py " + OPTIONS + " output " + f'"{event.src_path}"'
+            print(command)
+            subprocess.run([command], shell=True)
+            print("Conversion successful, manga is now available in output")
 if __name__ == "__main__":
     observer = Observer()
     event_handler = MyHandler(patterns=["*.cbz"])
-    observer.schedule(event_handler, path=path, recursive=True)
+    observer.schedule(event_handler, path="input", recursive=True)
     observer.start()
     try:
         while True:
